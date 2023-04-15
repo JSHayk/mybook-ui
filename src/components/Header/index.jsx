4952dfee039204c6abea0f024ec6f07c1cb63084
JSHayk <jshaykdeveloper@gmail.com>
2023-04-15 15:30:30 +0400

    added some changes

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,20 @@
 import "./index.scss"
 import basketIcon from "../../assets/icons/basket.svg";
+import Nav from "../Nav";
 
 const Header = () => {
   return (
     <header className="header">
-      <div className="header-logo">
-        <h1>MYBOOK</h1>
-        <div className="icon">
-          <img src={basketIcon} alt="basket" />
+      <div className="wrapper">
+        <div className="header-logo">
+          <h1>MYBOOK</h1>
+          <div className="icon">
+            <img src={basketIcon} alt="basket" />
+          </div>
+        </div>
+        <div className="row">
+          <Nav />
+          <button className="login">Log in</button>
         </div>
       </div>
     </header>
---
+++ src/components/Nav/index.jsx
@@ -0,0 +1,15 @@
+import "./index.scss"
+
+const Nav = () => {
+    return (
+        <nav className="nav">
+            <ul>
+                <li>Explorer</li>
+                <li>Shop</li>
+                <li>Blog</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav
